197ca02ac4f4f5419dcb8a76d3788975ece33d9b
tim <timothy.potts@colorado.edu>
2019-12-12 19:41:36 -0700

    made schema file

---
+++ schema.sql
@@ -0,0 +1,26 @@
+DROP DATABASE IF EXISTS companyDB;
+CREATE DATABASE companyDB;
+
+USE companyDB;
+
+CREATE TABLE employees (
+  id int AUTO_INCREMENT,
+  first_name varchar(30) NOT NULL,
+  last_name varchar(30) NOT NULL,
+  role_id int NOT NULL,
+  manager_id int NOT NULL,
+  PRIMARY KEY(id)
+);
+
+CREATE TABLE role (
+  id int AUTO_INCREMENT,
+  title varchar(30),
+  salary decimal,
+  department_id int,
+  PRIMARY KEY(id)
+);
+
+CREATE TABLE department (
+  id int AUTO_INCREMENT,
+  name varchar(30)
+);
