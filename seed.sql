91778fc56fbe376c4f28890f2855eac6b49432df
tim <timothy.potts@colorado.edu>
2019-12-30 16:26:13 -0700

    init

---
+++ seed.sql
@@ -0,0 +1,5 @@
+INSERT INTO employees (first_name, last_name, role_id, manager_id)
+VALUES ("Jerry", "Seinfeld", 420, 69),  ("Elaine", "Bennes", 30, 90), ("Cosmo", "Kramer", 30, 69), ("George", "Costanza", 30, 69), ("Frank", "Costello", 420, 4), ("Collin", "Sullivan", 1, 4);
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("Boss", 1000000, 420), ("")
