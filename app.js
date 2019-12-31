c005354d8e6886d2e3aca4f1ac9af7690312cffc
tim <timothy.potts@colorado.edu>
2019-12-30 17:10:57 -0700

    main menu started

--- app.js
+++ app.js
@@ -1,1 +1,70 @@
-const inquirer = require('inquirer');
+const inquirer = require("inquirer");
+const mysql = require("mysql");
+
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "a0E24me",
+  database: "employeeDB"
+});
+
+connection.connect(function(err) {
+  if (err) throw err;
+  console.log("connected as id " + connection.threadId + "\n");
+  createProduct();
+});
+
+function menu() {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "main",
+        message: "Select an option:",
+        choices: [
+          "View database tables",
+          "Update database tables",
+          "Add employee, department, or role"
+        ]
+      }
+    ])
+    .then(answers => {
+      if (answers.main === "View database tables") {
+        viewDatabase();
+      } else if (answers.main === "Update database tables") {
+        updataDatabase();
+      } else {
+        addObject();
+      }
+    });
+}
+
+function viewDatabase() {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "display",
+        message: "What table would you like to see? :",
+        choices: ["Employees", "Roles", "Departments"]
+      }
+    ])
+    .then(answers => {
+      if (answers.display === "Employees") {
+        //display employee table
+      } else if (answers.display === "Roles") {
+        //display roles
+      } else {
+        //display department
+      }
+    });
+}
+
+function updateDatabase() {
+  //to be determined
+}
+
+function addObject() {
+  //one sec
+}
--- schema.sql
+++ schema.sql
@@ -3,24 +3,36 @@ CREATE DATABASE companyDB;
 
 USE companyDB;
 
-CREATE TABLE employees (
-  id int AUTO_INCREMENT,
-  first_name varchar(30) NOT NULL,
-  last_name varchar(30) NOT NULL,
+CREATE TABLE employees
+(
+  id int
+  AUTO_INCREMENT,
+  first_name varchar
+  (30) NOT NULL,
+  last_name varchar
+  (30) NOT NULL,
   role_id int NOT NULL,
   manager_id int NOT NULL,
-  PRIMARY KEY(id)
+  PRIMARY KEY
+  (id)
 );
 
-CREATE TABLE role (
-  id int AUTO_INCREMENT,
-  title varchar(30),
+  CREATE TABLE roles
+  (
+    id int
+    AUTO_INCREMENT,
+  title varchar
+    (30),
   salary decimal,
   department_id int,
-  PRIMARY KEY(id)
+  PRIMARY KEY
+    (id)
 );
 
-CREATE TABLE department (
-  id int AUTO_INCREMENT,
-  name varchar(30)
+    CREATE TABLE department
+    (
+      id int
+      AUTO_INCREMENT,
+  name varchar
+      (30)
 );
